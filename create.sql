d69920e5705014ecdca87959cf914e7c11f419b7
Unknown <Josrodjr@gmail.com>
2018-04-23 11:42:07 -0600

    schema.sql para crear la db

---
+++ create.sql
@@ -0,0 +1,45 @@
+drop table if exists Usuario;
+create table Usuario(
+	id_usuario serial not null,
+	nombre_usuario varchar(25),
+	contrasena varchar(15),
+	correo_electronico varchar (30),
+	PRIMARY KEY (id_usuario)
+);
+
+drop table if exists Amigos;
+create table Amigos(
+	id_usuario1 integer,
+	id_usuario2 integer,
+	fecha_amistad date,
+	foreign key (id_usuario1) references Usuario(id_usuario),
+	foreign key (id_usuario2) references Usuario(id_usuario)
+);
+
+drop table if exists Tarea;
+create table Tarea(
+	id_tarea serial not null,
+	id_usuario integer,
+	nombre_tarea varchar(20),
+	fecha_finaliza date,
+	realizado boolean,
+	primary key (id_tarea),
+	foreign key (id_usuario) references Usuario(id_usuario)
+);
+
+drop table if exists Leaderboard;
+create table Leaderboard(
+	id_usuario integer,
+	puntos integer,
+	foreign key (id_usuario) references Usuario(id_usuario)
+);
+
+drop table if exists Mensajes;
+create table Mensajes(
+	emisor_id integer,
+	receptor_id integer,
+	mensaje varchar(140),
+	fecha_enviado date,
+	foreign key (emisor_id) references Usuario(id_usuario),
+	foreign key (receptor_id) references Usuario(id_usuario)
+);
