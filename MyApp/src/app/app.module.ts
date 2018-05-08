a87b3b36790a211af213d2f5873017f48b0834a5
Cpzty <Cristianpzty@gmail.com>
2018-05-07 20:07:30 -0600

    pagina despues del login

--- MyApp/src/app/app.module.ts
+++ MyApp/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { FirstPage } from '../pages/first/first';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    FirstPage
+
     
   ],
   imports: [
@@ -21,7 +23,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    FirstPage
     
   ],
   providers: [
--- MyApp/src/pages/home/home.ts
+++ MyApp/src/pages/home/home.ts
@@ -2,7 +2,7 @@ import { Component,ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { App, MenuController, AlertController } from 'ionic-angular';
-
+import {FirstPage} from '../first/first' ;
 
 
 @Component({
@@ -28,12 +28,28 @@ export class HomePage {
   	if(this.uname.value=="admin" && this.password.value=="admin"){
 
   		 let alert = this.alertCtrl.create({
+      title: 'Sign In Request',
+      subTitle: 'This page is under construction!',
+      buttons: ['OK']
+    });
+    alert.present();
+
+  	}
+  }
+
+   LogIn(){
+    //console.log(this.uname.value,this.password.value); 
+    if(this.uname.value=="admin" && this.password.value=="admin"){
+
+       let alert = this.alertCtrl.create({
       title: 'Login Succesful!',
       subTitle: 'You are logged in',
       buttons: ['OK']
     });
     alert.present();
-  	}
+    this.navCtrl.push(FirstPage);
+    
+    }
   }
 
 }
