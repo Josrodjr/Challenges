d912fb2a415166532930212d12827261b1bb8b1b
Unknown <Josrodjr@gmail.com>
2018-05-13 23:31:22 -0600

    Cambios en sql para sqlite

--- create.sql
+++ create.sql
@@ -9,9 +9,11 @@ create table Usuario(
 
 drop table if exists Amigos;
 create table Amigos(
+	id_relacion serial not null,
 	id_usuario1 integer,
 	id_usuario2 integer,
 	fecha_amistad date,
+	primary key (id_relacion),
 	foreign key (id_usuario1) references Usuario(id_usuario),
 	foreign key (id_usuario2) references Usuario(id_usuario)
 );
@@ -29,17 +31,21 @@ create table Tarea(
 
 drop table if exists Leaderboard;
 create table Leaderboard(
+	id_board serial not null,
 	id_usuario integer,
 	puntos integer,
+	primary key(id_board),
 	foreign key (id_usuario) references Usuario(id_usuario)
 );
 
 drop table if exists Mensajes;
 create table Mensajes(
+	id_mensaje serial not null,
 	emisor_id integer,
 	receptor_id integer,
 	mensaje varchar(140),
 	fecha_enviado date,
+	primary key (id_mensaje),
 	foreign key (emisor_id) references Usuario(id_usuario),
 	foreign key (receptor_id) references Usuario(id_usuario)
 );
